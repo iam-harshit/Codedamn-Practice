4432409e4e9d522582a14897ce826f2e5cfc2cf9
iam-harshit <importantemail545@gmail.com>
2024-01-21 18:13:40 +0530

    JavaScript Objects Lab

---
+++ JavaScript Objects Lab/index.js
@@ -0,0 +1,13 @@
+// Create your product object
+export const product = {
+    id: 1,
+    name: "Shoe",
+    price: 250,
+    availability: true,
+};
+
+// export your product object
+
+// console.log the object to view the output
+// TIP: Use the run button to run your JS Code
+console.log(product);
