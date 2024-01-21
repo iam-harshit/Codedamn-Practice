5111bd120f0fab82d1642aef6a0a4b58c6a5e082
iam-harshit <importantemail545@gmail.com>
2024-01-21 18:00:02 +0530

    Find the Bug using JavaScript

---
+++ Find the Bug/index.js
@@ -0,0 +1,11 @@
+
+// Fix the Bug!
+
+// const company = "Nike"
+// const tagline = "Just do it"
+const company = "Nike";
+const tagline = "Just do it";
+
+[company, tagline].forEach(
+	(el) => console.log(el)
+);
